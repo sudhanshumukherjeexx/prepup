3b8c467a7af371e9d4c181a555618c25c00fdaca
Sudhanshu Mukherjee <sudhanshumukherjeexx@gmail.com>
2023-05-03 14:18:17 -0400

    updated

--- README.md
+++ README.md
@@ -5,13 +5,88 @@
 [![image](https://img.shields.io/conda/vn/conda-forge/prepup.svg)](https://anaconda.org/conda-forge/prepup)
 
 
-**Prepup is a free, open-source package that lets you open, explore, visualize and pre-process datasets in your computer's terminal.**
+![image](C:\Users\Asus\prepup\logo.png)
 
+### Prepup is a free open-source package that lets you inspect, explore, visualize, and perform pre-processing tasks on datasets in your computer’s terminal.
+
+## Installation
+-   Prepup can be installed using the Pip package manager.
+
+### !pip install prepup
+
+## Motivation
+- Developing an efficient and user-friendly command line tool for data pre-processing to handle various tasks such as missing data, data formatting, and cleaning, with a simple interface and scalability for large datasets.
+
+## File Format Supported
+-   CSV
+-   EXCEL
+-   PARQUET
+
+## Why you should use Prepup?
+
+### It's Super fast
+-   Prepup is built on Polars which is alternative to pandas and helps you load and manipulate the DataFrames faster.
+
+### Analytical 
+-   Prepup handles tasks ranging from shape of data to the Standardizing the feature before training the model. It does it right on the terminal.
+
+### Compatible 
+-   Prepup supports CSV, EXCEL and PARQUET formats making it compatible to go with different file formats.
+
+### Non Destructive 
+-   Prepup doesn't alters your raw data, It saves pre-processed data only when user specifies the path.
+
+### Lives in your Terminal
+-   Prepup is terminal based and has specific entry points designed for using it instantly.
+
+# Command Line Arguments available in PREPUP
+
+## prepup <File name or File path> -inspect
+
+-   inspect flag takes the dataframe and returns the Features available, Features datatype and missing values present in the Dataset.
+
+## prepup <File name or File path> -explore
+
+-   explore flag takes the dataframe and returns the Features available, Features datatype, Correlation between features, Detects Outliers, Checks Normal Distribution, Checks Skewness, Checks Kurtosis and also allows the option to check if the dataset is Imbalanced.
+
+## prepup <File name or File path> -explore
+
+-   visualize flag plots the feature distribution directly on the terminal.
+
+## prepup <File name or File path> -visualize
+
+-   visualize flag plots the feature distribution directly on the terminal.
+
+## prepup <File name or File path> -impute
+- There are 8 different strategies available to impute missing data using Prepup
+
+    - Option 1 – Drops the Missing Data
+    - Option 2 – Impute Missing values with a Specific value
+    - Option 3 – Impute Missing values with Mean.
+    - Option 4 – Impute Missing values with Median.
+    - Option 5 – Impute Missing value based on the distribution of existing columns.
+    - Option 6 – Impute Missing values based on Forward Fill Strategy where missing values are imputed based on the previous data points.
+    - Option 7 - Impute Missing values based on Backward Strategy where missing values are imputed based on the next data points.
+    - Option 8 – Impute missing values based on K-Nearest Neighbors.
+
+## prepup <File name or File path> -standardize
+
+-   Standardize allows you to standardize the dataset using two different methods:
+    1. Robust Scaler
+
+    2. Standard Scaler 
+
+-   Robust Scaler is recommended if there are outliers present and you feel they can have influence on the Machine Learning model.
+
+-   Standard Scaler is go to function if you want to standardize the dataset before training the model on it.
+
+
+# License
 
 -   Free software: MIT license
+
+# Package Link
+-   Github: https://github.com/sudhanshumukherjeexx/prepup
+
 -   Documentation: https://sudhanshumukherjeexx.github.io/prepup
     
-
-## Features
-
--   TODO
--- prepup/__init__.py
+++ prepup/__init__.py
@@ -20,14 +20,6 @@ def parse_args():
     parser.add_argument('-impute', action='store_true', help='Impute Missing values.')
     parser.add_argument('-standardize', action='store_true', help='Standardize Feature Columns.')
     parser.add_argument('-rii', action='store_true', help='Convert code to Python Notebook.')
-    # parser.add_argument('-fmissing', action='store_true', help='Handle Missing data')
-    # parser.add_argument('-fhist', action='store_true', help='Feature Distribution in the Dataset')
-    # parser.add_argument('-fdist', action='store_true', help='Plots Features Distribution')
-    # parser.add_argument('-fscaling', action='store_true', help='Feature Scaling')
-    # parser.add_argument('-nd', action='store_true', help='Check Normal Distribution')
-    # parser.add_argument('-id', action='store_true', help='Check if Dataset is Imbalanced.')
-    # parser.add_argument('-pnotebook', action='store_true', help='Creates a Jupyter Notebook of all the functions displayed on Terminal')
-    # parser.add_argument('-rii', action='store_true', help='Global Dataset')
     return parser.parse_args()
 
 def load_file(file_path):
@@ -200,92 +192,7 @@ def main():
         print(colored(h_8.renderText("\nExecution Time for loading and inspecting data..."),'light_blue'))
         end_n = time.time()-start
         print("Operations Comepleted Successfully in {0} {1}".format(end_n,"seconds"))
+                   
 
-
-
-    # if args.fdist:
-    #     h_6 = Figlet(font='term')
-    #     print(colored(h_6.renderText("\nRelationship visualized between two variables..."),'light_blue'))
-    #     crafter =  Prepup(df)
-    #     crafter.scatter_plot()
-    # elif args.fhist:
-    #     crafter =  Prepup(df)
-    #     h_6 = Figlet(font='term')
-    #     print(colored(h_6.renderText("\nDistribution of Each Feature..."),'light_blue'))
-    #     time.sleep(0.5)
-    #     crafter.plot_histogram()
-    # elif args.pnotebook:
-    #     notebook_creator=NotebookCreator()
-    #     h_2 = Figlet(font='term')
-    #     print(colored(h_2.renderText("\nCreating Jupyter Notebook..."), 'light_blue'))
-    #     time.sleep(0.5)
-    #     notebook_creator.create_notebook()
-    # elif args.fscaling:
-    #     crafter =  Prepup(df)
-    #     h_6 = Figlet(font='term')
-    #     print(colored(h_6.renderText("\nFeature Scaling in Progress..."),'light_blue'))
-    #     time.sleep(0.2)
-    #     crafter.feature_scaling()
-    # elif args.fmissing:
-    #     crafter =  Prepup(df)
-    #     h_6 = Figlet(font='term')
-    #     print(colored(h_6.renderText("\nHandle Missing Data..."),'light_blue'))
-    #     time.sleep(0.2)
-    #     crafter.handle_missing_values()
-    # elif args.nd:
-    #     crafter =  Prepup(df)
-    #     h_6 = Figlet(font='term')
-    #     print(colored(h_6.renderText("\nCheck for Normal Distribution..."),'light_blue'))
-    #     time.sleep(0.2)
-    #     crafter.check_nomral_distrubution()
-    # elif args.rii:
-    #     missing_data = pl.read_parquet('missing_data.parquet')
-    #     missing_data = missing_data.to_pandas()
-    #     print(missing_data.isnull().sum())
-    #     print(missing_data)
-    # elif args.id:
-    #     crafter =  Prepup(df)
-    #     h_6 = Figlet(font='term')
-    #     print(colored(h_6.renderText("\nCheck for Imabalanced Dataset..."),'light_blue'))
-    #     time.sleep(0.2)
-    #     crafter.imbalanced_dataset()
-    # else:
-    #     start = time.time() 
-
-    #     crafter = Prepup(df)
-    #     h_1 = Figlet(font='term')
-    #     print(colored(h_1.renderText("Features present in the Dataset..."), 'light_blue')) 
-    #     time.sleep(0.5)
-    #     print(crafter.features_available())
-        
-    #     h_2 = Figlet(font='term')
-    #     print(colored(h_2.renderText("\nDatatype of each Feature..."), 'light_blue'))
-    #     time.sleep(0.5)
-    #     print(crafter.dtype_features())
-
-    #     h_3 = Figlet(font='term')
-    #     print(colored(h_3.renderText("\nShape of Data..."),'light_blue'))
-    #     time.sleep(0.5)
-    #     print(crafter.shape_data())
-                
-    #     h_4 = Figlet(font='term')
-    #     print(colored(h_4.renderText("\nFeatures: Missing values count..."),'light_blue'))
-    #     time.sleep(0.5)
-    #     crafter.missing_plot()
-        
-    #     h_9 = Figlet(font='term')
-    #     print(colored(h_9.renderText("\nDetecting Outliers..."),'light_blue'))
-    #     crafter.find_outliers()
-
-    #     h_5 = Figlet(font='term')
-    #     print(colored(h_5.renderText("\nFeature Correlation..."),'light_blue'))
-    #     time.sleep(0.5)
-    #     crafter.correlation_n()
-
-    #     h_8 = Figlet(font='term')
-    #     print(colored(h_8.renderText("\nExecution Time..."),'light_blue'))
-    #     end_n = time.time()-start
-    #     print("Operations Comepleted Successfully in {0} {1}".format(end_n,"seconds"))
-                
 if __name__ == '__main__':
      main()
